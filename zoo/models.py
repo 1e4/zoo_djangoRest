391d3a4d4c462e256be32256e85d8c70252f97ee
zeyytas <zzeynepp.zt@gmail.com>
2018-02-14 16:14:47 +0300

    '6'

--- zoo/models.py
+++ zoo/models.py
@@ -4,7 +4,7 @@ from pygments.lexers import get_lexer_by_name
 from pygments.formatters.html import HtmlFormatter
 from pygments import highlight
 
-choices =(('Lion','lion'),('Elephant','elephant'),('Monkey','monkey'),('Flamingo','flamingo'),('Giraffe','giraffe'))
+choices = (('Lion','lion'),('Elephant','elephant'),('Monkey','monkey'),('Flamingo','flamingo'),('Giraffe','giraffe'))
 
 class Cage(models.Model):
     name = models.CharField(max_length=20, null=True, blank=False, unique=True)
--- zoo/views.py
+++ zoo/views.py
@@ -1,6 +1,5 @@
 
 from .models import Animal,Cage
-from django.db.models import F, Q
 from .serializers import AnimalSerializer,CageSerializer, UserSerializer
 from rest_framework.decorators import detail_route
 from rest_framework.response import Response
@@ -9,51 +8,61 @@ from rest_framework import permissions, renderers, viewsets
 from .permissions import IsOwnerOrReadOnly
 from rest_framework.decorators import api_view
 from rest_framework.reverse import reverse
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse ,JsonResponse
+from django.http import Http404
 
 
 
 @api_view(['GET'])
-def api_root(request,format=None): #:8000/ de cikan linkleri tanimladik
+def api_root(request,format=None):
     return Response({
         'Animal': reverse('animal-list', request=request, format=format),
-        'Cage': reverse('cage-list', request=request, format=format),
-
-    })
+        'Cage': reverse('cage-list', request=request, format=format),})
 
 
 
 class AnimalViewSet(viewsets.ModelViewSet):
 
     queryset = Animal.objects.all()
-
     serializer_class = AnimalSerializer
     permission_classes = (
         permissions.IsAuthenticatedOrReadOnly,
         IsOwnerOrReadOnly,)
 
 
-
     def perform_update(self, serializer):
 
-        x = self.get_object()# animal model
-        first_cage = self.get_object().cid.pk
+        last_cage = self.request.POST['cid'][-2] # the cage name we want to put animal in it
+        newSizeOfCage = len(Cage.objects.filter(name=last_cage)[0].animals.all()) + 1  # looking for the new size after put the animal
+        size = Cage.objects.filter(name=last_cage)[0].size # the old size of the cage
 
-        last_cage = self.request.POST['cid'][-2]
+        value = 0
+        if newSizeOfCage <= size: # if there is a empty space
 
-        newSizeOfGace = len( Cage.objects.filter(name=last_cage)[0].animals.all())+1
+            if self.request.POST['type'] == 'Lion':# if the animal is lion
+                x = 'Flamingo'
+            if self.request.POST['type'] == 'Flamingo':
+                x = 'Lion'
+            else:  # for other animals, just save it
+                serializer.save(owner=self.request.user)
 
-        size = Cage.objects.filter(name=last_cage)[0].size
+            for ty in Cage.objects.filter(name=last_cage)[0].animals.all():  # look for is there any flamingo in the cage
 
-        if newSizeOfGace <= size:
-            Cage.objects.filter(name=first_cage)[0].size-=1
-            import pdb;pdb.set_trace()
-            serializer.save()
-        else:
-            #x.cid.pk = first_cage
+                if ty.type == x :
+                    value += 1
+            if value == 0:  # there is no flamingo or lion
+                import pdb;
+                pdb.set_trace()
+                serializer.save(owner=self.request.user)  # save it
+            else:  # so there is flamingo
+                import pdb;
+                pdb.set_trace()
+                raise Http404("Pick another cage.")
 
-            return HttpResponse('The cage is full')
+
+
+        else:  # if there is no emty space
+            raise Http404("The cage is full. Pick an another cage")
 
 
 
@@ -63,10 +72,37 @@ class AnimalViewSet(viewsets.ModelViewSet):
 
         return Response(Animal.highlighted)
 
+    '''' YOU CAN'T PUT LIONS AND FLAMINGOS IN THE SAME CAGE! '''
     def perform_create(self,  serializer):
-        queryset = Animal.objects.filter()
-        import pdb;pdb.set_trace()
-        serializer.save(owner=self.request.user)
+        cage = self.request.POST['cid'][-2]
+        newSizeOfCage = len(Cage.objects.filter(name=cage)[0].animals.all()) + 1 # looking for the new size after put the animal
+        size = Cage.objects.filter(name=cage)[0].size # the old size of the cage
+        value = 0
+        if newSizeOfCage <= size: # if there is a empty space
+            if self.request.POST['type'] == 'Lion': # if the animal is lion
+                for ty in Cage.objects.filter(name=cage)[0].animals.all():  # look for is there any flamingo in the cage
+                    if ty.type == 'Flamingo':
+                        value += 1
+                if value==0: # there is no flamingo
+                    import pdb;pdb.set_trace()
+                    serializer.save(owner=self.request.user) # save it
+                else: # so there is flamingo
+                    raise Http404("There is flamingo in the cage. Pick another cage.")
+
+            if self.request.POST['type'] == 'Flamingo': # if the animal is flamingo
+                for type in Cage.objects.filter(name=cage)[0].animals.all():  # look for is there any lion in the cage
+                    if type == 'Lion':
+                        value += 1
+                if value == 0:  # there is no lion
+                    serializer.save(owner=self.request.user)  # save it
+                else: # so there is lion
+                    raise Http404("There is lion in the cage. Pick another cage.")
+            else: # for other animals, just save it
+                serializer.save(owner=self.request.user)
+
+        else: # if there is no emty space
+            raise Http404("The cage is full. Pick an another cage")
+
 
 
 
